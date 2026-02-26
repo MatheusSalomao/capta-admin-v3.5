adb4bd88de24f94aeb5ad109f984d8cbf50dd35c
Matheus Salomão <contatomatheusss@gmail.com>
2026-02-26 16:42:46 -0500

    WIP: Add detailed informational panels and tabs to the Journey page
    Introduce new UI components to the Journey page, including multiple tabs (Informações, Avaliações, Matrícula, Documentos, Contratos) and detailed informational panels covering fields like course details, enrollment data, and financials. Update layout and styles to improve structure and readability.

--- src/app/pages/journey/journey.component.ts
+++ src/app/pages/journey/journey.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { MatCard, MatCardContent } from '@angular/material/card';
 import { AsideComponent } from '@app/pages/journey/lead-aside/aside.component';
+import { MatTab, MatTabGroup, MatTabLabel } from '@angular/material/tabs';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-journey',
-  imports: [MatCard, MatCardContent, AsideComponent],
+  imports: [MatCard, MatCardContent, AsideComponent, MatTabGroup, MatTab, MatTabLabel, MatIcon],
   templateUrl: './journey.component.html',
   styleUrl: './journey.component.scss',
 })
